a82cab86e9af4d65e3625f5a7ddf88fcdc948a44
Elias Jr <elijah.junnior@outlook.com>
2022-05-09 08:04:50 -0300

    memo added on ProductItem component to improove your perform

--- pages/components/ProductItem.tsx
+++ pages/components/ProductItem.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react"
+
 type ProductItemProps = { 
   product: { 
     id: number, 
@@ -6,10 +8,25 @@ type ProductItemProps = {
   }
 }
 
-export function ProductItem({ product } : ProductItemProps) {
+function ProductItemComponent({ product } : ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   )
 }
+
+// usando a função memo para evitar que o componente 
+// sejá renderização novamente sem que nenhuma das suas
+// propriedades seja alterada
+export const ProductItem = memo(
+  ProductItemComponent, 
+  // passando uma funçao que permite personalizar 
+  // a comparação das propriedades
+  (prevProps, nextProps) => { 
+    // Usando a função is da classe Object para fazer 
+    // uma comparação profunda nos dois objetos (versão 
+    // anterior e nova das propriedades)
+    return Object.is(prevProps.product, nextProps.product)
+  }
+)
